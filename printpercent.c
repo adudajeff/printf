a2f57bb163b4313e26a5caa5359d70844b8b1df6
Geofrey Aduda <adusoftjeff@gmail.com>
2023-04-22 01:30:27 -0700

    printf

---
+++ printpercent.c
@@ -0,0 +1,15 @@
+#include "h.c"
+
+/**
+ * printpercent - prints %
+ * @format: format
+ * @pa: va_list
+ * Return: number of characters printed
+ */
+int printpercent(char *format, va_list pa)
+{
+	(void)format;
+	(void)pa;
+	_putchar('%');
+	return (1);
+}
